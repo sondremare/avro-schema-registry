d1d32ccecfc542af626e6cf0ecfb030c1d9d2142
sondremare <sondremo@gmail.com>
2020-02-26 10:13:48 +0100

    feat: add support for referencing other avro schemas in an avro schema
    How is this done? you pass in schemaDependencies when encoding a message, this will upload the combined schema to a schema registry, and load the dependencies in cache.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare namespace schema {
     export type DecodeMessage = <T>(obj: Buffer, parseOptions?: any) => Promise<T>;
     export type EncodeById = (schemaId: number, msg: any, parseOptions?: any) => Promise<Buffer>;
-    export type EncodeBySchema = (topic: string, schema: any, msg: any, parseOptions?: any) => Promise<Buffer>;
+    export type EncodeBySchema = (topic: string, schema: any, msg: any, parseOptions?: any, schemaDependencies?: any[]) => Promise<Buffer>;
     export type EncodeByTopicName = (topic: string, msg: any, parseOptions?: any) => Promise<Buffer>;
     export type GetSchemaByTopicName = (topic: string, parseOptions?: any) => Promise<{id: number, parsedSchema: any}>;
 
